4dbba40ef0dc670ea7808d4f7673f77aefe4160f
d-herbert <deidre.m.herbert@gmail.com>
2022-03-05 14:50:23 -0800

    triangle loop

---
+++ js/03-triangle.js
@@ -0,0 +1,9 @@
+let counter = 0
+
+// display
+document.write('<h3> welcome to the `triangle loop app` 🌵</h3><br>')
+
+for (let i = 1; i < 8; i++) {
+    counter += 1
+    console.log('#'.repeat(counter))  
+}
