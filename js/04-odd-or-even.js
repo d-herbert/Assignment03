9f476e0812138be2d6c190b892905faeba008b54
d-herbert <deidre.m.herbert@gmail.com>
2022-03-05 14:56:59 -0800

    odd / even app complete

---
+++ js/04-odd-or-even.js
@@ -0,0 +1,10 @@
+// display
+document.write('<h3> welcome to the `odd or even app` 🌟</h3><br>')
+
+for (let i = 0; i < 16; i++) {
+    if (i % 2 == 0) {
+        console.log(`${i} is even`) 
+    } else {
+        console.log(`${i} is odd`)
+    }
+}
